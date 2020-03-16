328cec3b62b6c43702e5d75698a3587bc110f432
Suyash08Oct <35406637+Suyash08Oct@users.noreply.github.com>
2020-03-16 15:36:28 +0530

    Initial Setup

---
+++ src/test/java/com/suyash/springsecurityjwt/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suyash.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
